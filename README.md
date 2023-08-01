24be449a49a6490414fb4ba689a59e08b4fc319e
louischausse-auxilio <lchausse@auxilio.io>
2023-08-01 15:10:03 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # hubspot-custom-coded-action-clean-comma-separated-string
+This custom coded action will:
+
+- remove duplicated words in a comma separated string
+- remove extra comma "," not preceded by a word or not followed by a word (", ,")
+- replace "_" by spaces
+- remove extra spaces
+- capitalize first letter of a list item
+
+For instance, processing this: _"Sales, marketing, Operations, , marketing, , Technology, Information technology, information_technology"_
+
+Would output this: _"Sales, Marketing, Operations, Technology, Information Technology"_
  
---
+++ clean-comma-separated-string.js
@@ -0,0 +1,25 @@
+exports.main = async (event, callback) => {
+    function processString(inputString) {
+      // Remove duplicated words
+      let uniqueWords = [...new Set(inputString.split(", ").map(word => word.replace("_", " ").toLowerCase()))];
+  
+      // Remove extra comma and filter out empty words
+      let processedString = uniqueWords.filter(word => word !== "").join(", ");
+  
+      // Capitalize first letter of each word
+      processedString = processedString.replace(/\b\w/g, char => char.toUpperCase());
+  
+      return processedString;
+    }
+  
+    const string = event.inputFields.formula_result_output // e.g. "Sales, marketing, Operations, , marketing, , Technology, Information technology, information_technology"
+    const cleanString = processString(string) // e.g. "Sales, Marketing, Operations, Technology, Information Technology"
+  
+    console.log(cleanString); 
+  
+    callback({
+      outputFields: {
+        cleanString: cleanString
+      }
+    });
+  }
